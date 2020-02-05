56fffb2107f64595e921fa34347016a432a3972a
Reavern <f.sujanto@gmail.com>
2020-02-05 15:15:02 +0700

    Implemented params

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,9 @@
-import '../assets/fonts/open_sans.css'
-import "../assets/vendor/nucleo/css/nucleo.css";
-import "../assets/vendor/@fortawesome/fontawesome-free/css/all.min.css";
-import "../assets/css/argon-dashboard-react.css";
+import '../../public/css/main.css';
 
-import 'aos/dist/aos.css';
 
-import { AnimatePresence, motion } from 'framer-motion';
-// AOS.init();
 export default function MyApp({ Component, pageProps }) {
 
   return (
-    // <AnimatePresence exitBeforeEnter={true}>
       <Component {...pageProps} />
-    // </AnimatePresence>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,133 +1,90 @@
 import React, { useState } from 'react';
 import { NextPage } from 'next';
-import Head from 'next/head'
-// import ResponsiveDrawer from '../components/ResponsiveDrawer';
-import { firebaseApp } from '../utils/Firebase';
-import { motion } from "framer-motion";
-
-import Link from 'next/link';
-
-import {
-    Button
-} from "reactstrap";
-import { CSSProperties } from '@material-ui/styles';
-
-const Item = ({ children }) => {
-
-    const itemStyle = {
-        background: 'black',
-        borderRadius: '30px',
-        width: '150px',
-        height: '150px',
-    } as CSSProperties;
-
-    return (
-        <div style={itemStyle}>
-            {children}
-        </div>
-    );
-}
+import Head from 'next/head';
+import { useRouter } from 'next/router'
+import QRCode from 'qrcode.react';
+// import QRCode from 'qrcode-react';
 
 const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => {
-
-    const [midtransToken, setMidtransToken] = useState('');
-
-    const itemStyle = {
-        background: 'black',
-        borderRadius: '30px',
-        width: '150px',
-        height: '150px',
-    } as CSSProperties;
-
-    async function createTransaction() {
-
-        const id = firebaseApp.firestore().collection('id').doc().id;
-
-        const parameter = {
-            "parameter": {
-                "transaction_details": {
-                    "order_id": id,
-                    "gross_amount": 200000
-                }, "credit_card": {
-                    "secure": true
-                }
-            }
-        }
-
-        const URL = 'https://nextjs-ts-template.now.sh/api/createTransaction';
-
-        try {
-            const res = await fetch(URL, {
-                method: 'POST',
-                body: JSON.stringify(parameter),
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*'
-                }
-            });
-
-            const json = await res.json();
-
-            if (json.error === 0) {
-                console.log(json);
-                setMidtransToken(json.data.token);
-            } else {
-                console.log(json.message);
-            }
-
-        } catch (e) {
-            console.log(e);
-        }
-
-
-    }
-
-    function snapButtonPressed() {
-        (window as any).snap.pay(midtransToken as any, {
-            onSuccess: function (result) {
-                console.log(result);
-            },
-            onPending: function (result) {
-                console.log(result);
-            },
-            onError: function (result) {
-                console.log(result);
-            }
-        });
-    }
-
+    const router = useRouter()
+    const { name, year, date, code } = router.query;
     return (
-        <html>
+        <>
             <Head>
-                <title>My page title</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+                <title>Ketna | QR Code</title>
 
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+
+                <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:600,800&display=swap" rel="stylesheet" />
+
+                <link rel="shortcut icon" href="public/img/logo.png" />
+
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" />
+
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
             </Head>
-            <body>
-                <h1>TEST : {userAgent}</h1>
-                <Link href="/admin"><a>ADMIN</a></Link><br />
-                <Button onClick={createTransaction}>CREATE MIDTRANS TRANSACTION</Button>
-                {midtransToken}
-                {
-                    midtransToken ?
-                        (
-                            <Button onClick={snapButtonPressed}>MIDTRANS SNAP</Button>
-                        ) : null
-                }
-                <div style={itemStyle}>
-                <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} />
+            <>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 qr-box">
+                            <div className="qr-box__content">
+                                <img src='/img/logo.png' className="img-fluid d-block mx-auto logo" />
+
+                                <div className="row mt-3 mt-md-4">
+                                    <div className="col-6">
+                                        <h3>Nama Undangan</h3>
+                                        <p>{name}</p>
+
+                                        <h3>Angkatan</h3>
+                                        <p>{year}</p>
+                                    </div>
+
+                                    <div className="col-6">
+                                        <h3>Hari / Tanggal</h3>
+                                        <p>{date}</p>
+
+                                        <h3>Jumlah Tiket</h3>
+                                        <p>1 (Satu)</p>
+                                    </div>
+
+                                    <div className="col-12 qr-code">
+                                        <div className="qr-code-container d-block mx-auto">
+                                            <QRCode
+                                                value={code}
+                                                size={200}
+                                                className="d-block mx-auto"
+                                                imageSettings={{
+                                                    src: '/img/logo_center.png',
+                                                    excavate: true,
+                                                    height: 27,
+                                                    width: 27
+                                                }}
+                                            />
+                                        </div>
+                                    </div>
+
+                                    {/* <div className="col-12 qr-code" style={{ backgroundColor: 'red' }}>
+
+                                        <QRCode
+                                            value={code}
+                                            className="d-block mx-auto"
+                                        />
+                                        <img src='/img/qr-code.png' className="d-block mx-auto" />
+                                    </div> */}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <Item>
-                    <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} />
-                </Item>
-                <script src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="SB-Mid-client-s3ZYCAV1B68etLnU"></script>
-            </body>
-        </html>
+
+
+            </>
+        </>
     );
 }
-
-Home.getInitialProps = async ({ req }) => {
-    const userAgent = req ? req.headers['user-agent'] : navigator.userAgent;
-    return { userAgent };
-};
 
 export default Home;
